73a30251833bfc9f8b703785764c62133b448bbc
surajpaul2412 <suraj.paul.69@gmail.com>
2024-06-23 21:37:46 +0530

    23 | login api setup

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+
+            // Check if the user has the role of "customer" (role_id 3) or "driver" (role_id 4)
+            if (in_array($user->role_id, [3, 4])) {
+                $token = $user->createToken('LaravelSanctumAuth')->plainTextToken;
+
+                return response()->json([
+                    'token' => $token,
+                    'role' => $user->role->name,
+                ], 200);
+            } else {
+                return response()->json(['error' => 'Login not allowed'], 401);
+            }
+        } else {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,10 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::post('/login', function (Request $request) {
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return response()->json(['error' => 'Invalid credentials'], 401);
-    }
-
-    $token = $user->createToken('api-token')->plainTextToken;
-
-    return response()->json(['token' => $token]);
 });
